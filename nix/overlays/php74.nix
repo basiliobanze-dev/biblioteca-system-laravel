7eaa61c5807a872745ab64d31ba9c7f3b7b04d1d
Basílio Banze <bbanze16@gmail.com>
2025-06-17 08:26:51 +0200

    REMOVENDO TODOS ARQUIVOS (nixpacks)

--- nix/overlays/php74.nix
+++
@@ -1,9 +0,0 @@
-self: super:
-{
-  php74 = super.callPackage (super.fetchFromGitHub {
-    owner = "NixOS";
-    repo = "nixpkgs";
-    rev = "um-commit-antigo-com-php74"; # Substitua por um commit válido
-    sha256 = "0000000000000000000000000000000000000000000000000000";
-  }) {};
-}
